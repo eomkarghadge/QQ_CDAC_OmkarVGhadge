ad54cfde83a7c2916f4fe88a1580d8a688eeb08f
eomkarghadge <omkarghadge71@gmail.com>
2024-04-02 15:09:04 +0530

    ChatApp

---
+++ react-chat-app-master/src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ChatDashboard from "./ChatDashboard";
+
+
+class App extends Component {
+    state = {
+        messages: [
+            {username: 'Pranav', text: 'Hi, Omkar!'},
+            {username: 'Pranav', text: 'How are you?'},
+            {username: 'Omkar', text: 'Hi, Pranav! How are you?'},
+        ],
+        users: [{username: 'Pranav'}, {username: 'Omkar'}]
+    };
+
+    updateMessageListing = (message) => {
+        let update_messages_list = this.state.messages
+        update_messages_list.push(message)
+
+        this.setState({
+            messages: update_messages_list,
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">ReactND - Coding Practice</h1>
+                </header>
+                <div className="container">
+                    {
+                        this.state.users.map(user => (
+                            <ChatDashboard
+                                key={user.username}
+                                user={user}
+                                updateMessages={this.updateMessageListing}
+                                messages={this.state.messages}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
